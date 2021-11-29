c1a4203269c0ca98dcd581d6166c2089901cd579
isaiah wright <s10780369@students.duvalschools.org>
2021-11-29 14:44:31 -0500

    Update MyfirstPygame.py

--- .github/MyfirstPygame.py
+++ .github/MyfirstPygame.py
@@ -1,1 +1,26 @@
-# My First PyGame, Isaiah Wright, 11/29/21 1:59pm, v0.0
+# My First PyGame, Isaiah Wright, 11/29/21 2:28pm, v0.0
+
+import pygame, sys
+from pygame.locals import *
+# start PyGame
+pygame.init()
+
+# setup our window. 1
+windowsurface = pygame.display.set_mode((500, 400), 0, 32) 
+pygame.display.set_caption('hello, world')
+
+# Setup Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 225, 225)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# Setup font.
+basicfont = pygame.font.SysFont(None, 48)
+
+# Setup text.
+text = basicfont.render('hello, World', True, WHITE, BLUE)
+textRect = text.get_rect()
+textRect.centerx = windowSurface.get_rect().centrax
+textRect.centery = windowSurface.get_rect().centery
