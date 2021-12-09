dfaab647f7453927edf41bdea380b128a64fc882
isaiah wright <s10780369@students.duvalschools.org>
2021-12-09 14:48:09 -0500

    Update simple_animation.py

--- .github/simple_animation.py
+++ .github/simple_animation.py
@@ -1,4 +1,4 @@
-# Simple Animation with pyGame, isaiah wright, 12/09/21, 2:33 pm v0.5
+# Simple Animation with pyGame, isaiah wright, 12/09/21, 2:47 pm v0.6
 
 import pygame, sys, time
 from pygame.locals import *
@@ -27,9 +27,9 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
 #setup the box data. 
-b1 = {'react':pygame.Rect(300, 80, 50, 100), 'color': RED, 'dir':UPRIGHT}
-b2 = {'react':pygame.Rect(200, 200, 20, 20), 'color': GREEN, 'dir':UPLEFT}
-b3 = {'react':pygame.Rect(100, 150, 60, 60), 'color': BLUE, 'dir':DOWNLEFT}
+b1 = {'rect':pygame.Rect(300, 80, 50, 100), 'color': RED, 'dir':UPRIGHT}
+b2 = {'rect':pygame.Rect(200, 200, 20, 20), 'color': GREEN, 'dir':UPLEFT}
+b3 = {'rect':pygame.Rect(100, 150, 60, 60), 'color': BLUE, 'dir':DOWNLEFT}
 boxes = [b1, b2, b3]
 
 # Run the game loop.
@@ -39,3 +39,22 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+
+    windowSurface.fill(WHITE)
+
+    for b in boxes:
+        # Move the box data structure.
+        if b['dir'] == DOWNLEFT:
+            b['rect'].left -= MOVESPEED
+            b['rect'].top += MOVESPEED
+        if b['dir'] == DOWNRIGHT:
+            b['rect'].left += MOVESPEED
+            b['rect'].top += MOVESPEED
+        if b['dir'] == UPLEFT:
+            b['rect'].left -= MOVESPEED
+            b['rect'].top -= MOVESPEED
+        if b['dir'] == UPRIGHT:
+            b['rect'].left += MOVESPEED
+            b['rect'].top -= MOVESPEED          
+
+
