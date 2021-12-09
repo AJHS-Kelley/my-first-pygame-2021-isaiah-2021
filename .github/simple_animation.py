c6a3ee91bae558b64b7d0afc90c571acbe312bda
isaiah wright <s10780369@students.duvalschools.org>
2021-12-09 14:33:40 -0500

    Update simple_animation.py

--- .github/simple_animation.py
+++ .github/simple_animation.py
@@ -1,4 +1,4 @@
-# Simple Animation with pyGame, isaiah wright, 12/09/21, 1:31 pm
+# Simple Animation with pyGame, isaiah wright, 12/09/21, 2:33 pm v0.5
 
 import pygame, sys, time
 from pygame.locals import *
@@ -31,3 +31,11 @@ b1 = {'react':pygame.Rect(300, 80, 50, 100), 'color': RED, 'dir':UPRIGHT}
 b2 = {'react':pygame.Rect(200, 200, 20, 20), 'color': GREEN, 'dir':UPLEFT}
 b3 = {'react':pygame.Rect(100, 150, 60, 60), 'color': BLUE, 'dir':DOWNLEFT}
 boxes = [b1, b2, b3]
+
+# Run the game loop.
+while True:
+    # Check for Quit event.
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
