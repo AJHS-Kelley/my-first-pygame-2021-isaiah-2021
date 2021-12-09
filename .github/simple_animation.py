756a6fafa2636d94bebf4c473cf9a648d166a368
isaiah wright <s10780369@students.duvalschools.org>
2021-12-09 14:09:13 -0500

    Update simple_animation.py

--- .github/simple_animation.py
+++ .github/simple_animation.py
@@ -1,1 +1,13 @@
 # Simple Animation with pyGame, isaiah wright, 12/09/21, 1:31 pm
+
+import pygame, sys, time
+from pygame.locals import *
+
+# Setup Pygame
+pygame.init()
+
+# Setup the window
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption('Animation Example!')
