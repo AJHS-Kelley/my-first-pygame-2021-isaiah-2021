31fcec1d96b46437859132c871ee61faa49959cd
isaiah wright <s10780369@students.duvalschools.org>
2021-12-09 14:19:46 -0500

    Update simple_animation.py

--- .github/simple_animation.py
+++ .github/simple_animation.py
@@ -11,3 +11,17 @@ WINDOWWIDTH = 400
 WINDOWHEIGHT = 400
 windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
 pygame.display.set_caption('Animation Example!')
+
+# Setup the direction variables.
+DOWNLEFT = 'downleft'
+DOWNRIGHT = 'downright'
+UPLEFT = 'upleft'
+UPRIGHT = 'upright'    
+
+MOVESPEED = 4
+
+# Setup color values
+WHITE =(255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
