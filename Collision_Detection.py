b41d906c65efe76e7f9da480290428db349c412e
isaiah wright <77157335+isaiah-2021@users.noreply.github.com>
2022-01-07 14:11:32 -0500

    Update Collision_Detection.py

--- Collision_Detection.py
+++ Collision_Detection.py
@@ -1,1 +1,8 @@
 # Pygame Collision Dection Practice, Isaiah Wright, January 07, 2022, 2:00pm
+
+import pygame, sys, random
+from pygame.locals import 
+
+# Setup Pygame
+pygame.init()
+mainClock = pygame.time.Clock()
