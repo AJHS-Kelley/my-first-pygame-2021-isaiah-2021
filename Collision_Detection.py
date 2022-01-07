94404753ee5b2048a94064f953a47a57e0e641e2
isaiah wright <77157335+isaiah-2021@users.noreply.github.com>
2022-01-07 14:18:39 -0500

    Update Collision_Detection.py

--- Collision_Detection.py
+++ Collision_Detection.py
@@ -6,3 +6,16 @@ from pygame.locals import
 # Setup Pygame
 pygame.init()
 mainClock = pygame.time.Clock()
+
+# Setup the Pygame Window
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+pygame.display.set_caption('Collision Detection 2022')
+
+# Setup colors.
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+WHITE = (255, 255, 255)
+
+
